45112f2cf3942df7386038a6b5da979667174cf4
FrankieFosse <146066047+FrankieFosse@users.noreply.github.com>
2025-06-09 20:18:09 +0200

    Added profile picture

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ function Profile() {
 
     return (
         <>
-        <div className="bg-whitePrimary/5 backdrop-blur flex flex-col lg:mx-24 py-4 lg:px-12 rounded-lg mt-8 justify-evenly items-center h-128 xl:flex-row">
+        <div className="bg-whitePrimary/5 backdrop-blur flex flex-col lg:mx-24 pt-4 lg:px-12 rounded-lg mt-8 justify-evenly items-center h-96 xl:flex-row">
             <div className="flex flex-col justify-center items-center xl:flex-row xl:w-1-3">
             <h1 className="text-4xl 2xl:text-5xl font-black bg-gradient-to-r from-greenPrimary to-bluePrimary text-transparent bg-clip-text brightness-200"
             style={{
@@ -13,14 +13,14 @@ function Profile() {
             </h1>
             </div>
               <div className="xl:w-1/3 flex flex-col justify-center items-center">
-            <img src="/images/User-avatar.png" 
-                 className="w-48 h-48"
+            <img src="/images/profile.png" 
+                 className="absolute h-48 top-48 object-cover top-0 brightness-90 sm:h-64 sm:top-32 sm:left-24 xl:left-0 xl:h-96 xl:top-0 xl:left-54"
             />
             </div>
 
-        <div className="flex flex-col justify-center items-center mx-4 xl:w-1/3">
+        <div className="flex flex-col justify-center items-center mx-4 xl:w-1/3 pb-40 sm:pl-64 sm:pb-10 xl:pl-0">
             <h2 className="text-xl lg:text-2xl font-bold">Frank Kristian Fosse</h2>
-            <p className="font-light text-whitePrimary/60 text-xs sm:w-2/4 lg:text-sm">
+            <p className="font-light text-whitePrimary/60 text-xs sm:w-2/4 lg:text-sm w-2/4 xl:w-full">
                 I'm a Front-End Developer with a passion for crafting modern, responsive, and user-friendly websites and applications.
             </p>
         </div>
